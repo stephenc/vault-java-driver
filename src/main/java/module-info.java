47e767d65366962d3b2e2638e74af97b8d14687d
Stephen Connolly <stephen.alan.connolly@gmail.com>
2019-09-06 17:37:19 +0100

    Remove module-info so we can build with jjava 8

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module vault.java.driver {
-    requires java.logging;
-    exports com.bettercloud.vault;
-    exports com.bettercloud.vault.api;
-    exports com.bettercloud.vault.json;
-    exports com.bettercloud.vault.response;
-    exports com.bettercloud.vault.rest;
-}
